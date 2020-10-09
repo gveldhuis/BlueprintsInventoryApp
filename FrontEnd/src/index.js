f612859b1209efcc3a369605cd17a7e16fb89422
Unser Hashsham <unserh@umich.edu>
2020-10-09 00:39:51 -0400

    Add initial directory structure

--- FrontEnd/src/index.js
+++ FrontEnd/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
 
 ReactDOM.render(
-    <h1>Blueprints Inventory App</h1>,
+    <App />,
     document.getElementById('root')    
 );
