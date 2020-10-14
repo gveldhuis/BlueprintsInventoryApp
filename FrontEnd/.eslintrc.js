bd3646d2cbec39b95cf0baeab306a606370dbae6
Unser Hashsham <unserh@umich.edu>
2020-10-13 23:46:55 -0400

    Add eslinter

---
+++ FrontEnd/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react",
+  ],
+  "env": {
+      "browser": true,
+  },
+
+  // Allow use of console.log()
+  "rules": {
+    "no-console": 0,
+  }
+};
