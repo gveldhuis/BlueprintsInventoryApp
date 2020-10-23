eda2f5d0c488726a4486b72c2f4588769a97ee52
fiahmed <fiahmed@umich.edu>
2020-10-23 14:04:01 -0400

    login updated

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -4,17 +4,23 @@ import Authentication from '../utils/Auth';
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      value: '',
-    };
+    this.state = {firstname: '', 
+      lastname: '',
+      email: '',
+      eventpass: '',
+      org: '',
+      event:''};
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   handleChange(event) {
-    this.setState({
-      value: event.target.value,
-    })
+    const target = event.target
+  	const value = target.value
+  	const name = target.name
+  	this.setState(
+  		{[name]: value}
+  	)
   }
 
   handleSubmit(event, login) {
@@ -23,23 +29,54 @@ class Login extends React.Component {
   }
 
   render() {
-    const { value } = this.state;
+    //const { value } = this.state;
     return (
-      <Authentication.Consumer>
-        {(auth) => (
-          <form onSubmit={(event) => this.handleSubmit(event, auth.setLogin)}>
+        <Authentication.Consumer>
+          {(auth) => (
+            <form onSubmit={(event) => this.handleSubmit(event, auth.setLogin)}>
+            <img src={require('./Login/logo192.png')} alt = "Logo" />
+              <label>
+            First Name:
             <input 
-              type="text"
-              value={value}
-              onChange={(event) => this.handleChange(event)}
-            />
-            <input
-              type="submit"
-              value="Login"
-            />
-          </form>
-        )}
-      </Authentication.Consumer>
+              name="firstname"
+              type="text" 
+              value={this.state.firstname} 
+              onChange={this.handleChange} />
+          </label>
+          <br />
+          <label>
+            Last Name:
+            <input 
+              name = "lastname"
+              type="text" 
+              value={this.state.lastname}
+              onChange={this.handleChange}/>
+          </label>
+          <br />
+          <label>
+            Email:
+            <input 
+              name = "email"
+              type = "text"
+              value={this.state.email}
+              onChange={this.handleChange}/>
+          </label>
+          <br />
+          <label>
+            Event Password:
+            <input 
+              name = "eventpass"
+              type = "text"
+              value={this.state.eventpass}
+              onChange={this.handleChange}/>
+          </label>
+              <input
+                type="submit"
+                value="Login"
+              />
+            </form>
+          )}
+        </Authentication.Consumer>
     );
   }
 }
