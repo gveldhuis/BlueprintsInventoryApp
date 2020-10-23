f3bb39fb6cb2e061d144fd3870d26b8c1f6e1123
Unser Hashsham <unserh@umich.edu>
2020-10-23 18:21:09 -0400

    Refactor sessions to be encrypted

--- FrontEnd/src/utils/session_utils.jsx
+++ FrontEnd/src/utils/session_utils.jsx
@@ -1,15 +1,23 @@
 import Cookies from 'universal-cookie';
+import AES from 'crypto-js/aes';
+import Utf8 from 'crypto-js/enc-utf8';
 
 const cookies = new Cookies();
+const key = process.env.REACT_APP_COOKIE_KEY;
 
-export const getCookie = (name) => {
-  return cookies.get(name);
+export const getSession = (name) => {
+  const cookie = cookies.get(name);
+  if (cookie === undefined) {
+    return null;
+  } else {
+    return AES.decrypt(cookies.get(name), key).toString(Utf8);
+  }
 }
 
-export const setCookie = (name, value) => {
-  cookies.set(name, value);
+export const setSession = (name, value) => {
+  cookies.set(name, AES.encrypt(value, key).toString());
 };
 
-export const  removeCookie = (name) => {
+export const clearSession = (name) => {
   cookies.remove(name);
 }
