6c21222bfc75ac2f30f3b70056662ac9626dc499
lydialam <lydialam@umich.edu>
2020-10-24 19:30:08 -0700

    styled Navbar and updated tailwind config

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -5,10 +5,10 @@ class Navbar extends React.Component {
   render() {
     return (
       <div>
-        <ul> 
-          <li><NavLink exact to="/stats"><i className="fas fa-chart-line"></i></NavLink></li>
-          <li><NavLink to="/camera"><i className="fas fa-camera"></i></NavLink></li>
-          <li><NavLink to="/help"><i className="fas fa-question-circle"></i></NavLink></li>
+        <ul class="bg-med_blue p-0 m-0 text-center flex justify-between w-full fixed bottom-0 rounded-t-lg"> 
+          <li class="text-light_blue focus-within:text-white font-bold no-underline p-5 pl-10 pr-10 flex text-3xl"><NavLink exact to="/stats"><i className="fas fa-chart-line"></i></NavLink></li>
+          <li class="text-light_blue focus-within:text-white font-bold no-underline p-5 pl-10 pr-10 flex text-3xl"><NavLink to="/camera"><i className="fas fa-camera"></i></NavLink></li>
+          <li class="text-light_blue focus-within:text-white font-bold no-underline p-5 pl-10 pr-10 flex text-3xl"><NavLink to="/help"><i className="fas fa-question-circle"></i></NavLink></li>
         </ul>
       </div>
     );
--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -4,13 +4,27 @@ module.exports = {
     fontFamily: {
       sans: ['Avenir', 'sans-serif'],
     },
+    borderRadius: {
+      'none': '0',
+      'sm': '0.125rem',
+      default: '0.25rem',
+      default: '4px',
+      'md': '0.375rem',
+      'lg': '10px',
+      'full': '9999px',
+      'large': '12px',
+    },
     extend: {
       colors: {
         light_blue: '#015eea',
         med_blue: '#003296',
         dark_blue: '#00193c',
         grey: '#bdbbbb',
+        white: '#fff',
       },
     }
+  },
+  variants: {
+    textColor: ['responsive', 'hover', 'focus', 'focus-within'],
   },
 }
