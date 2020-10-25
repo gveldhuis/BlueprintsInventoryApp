8100f5a0a4c48595380d869dc7067e0db4e24eaa
Unser Hashsham <unserh@umich.edu>
2020-10-25 19:49:32 -0400

    Add tailwind reference component and custom tailwind utilities

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,10 +1,20 @@
 module.exports = {
   important: true,
   theme: {
-    fontFamily: {
-      sans: ['Avenir', 'sans-serif'],
-    },
     extend: {
+      spacing: {
+        sm: '0.5rem',
+        md: '1rem',
+        lg: '2rem',
+        xl: '4rem',
+      },
+      fontFamily: {
+        avenir: ['Avenir', 'sans-serif'],
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+      },
       colors: {
         light_blue: '#015eea',
         med_blue: '#003296',
