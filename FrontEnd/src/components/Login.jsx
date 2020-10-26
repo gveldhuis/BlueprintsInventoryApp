51b4c029bd2f8bbbbb0af7275aee7b2b3059e1f1
Unser Hashsham <unserh@umich.edu>
2020-10-25 22:25:36 -0400

    Touch up styles on Login page

--- FrontEnd/src/components/Login.jsx
+++ FrontEnd/src/components/Login.jsx
@@ -33,48 +33,107 @@ class Login extends React.Component {
     return (
         <Authentication.Consumer>
           {(auth) => (
-            <form onSubmit={(event) => this.handleSubmit(event, auth.setLogin)}>
-            <img src={require('assets/images/Blueprints_Logo3.png')} alt = "Logo" />
-              <label>
-            First Name:
-            <input 
-              name="firstname"
-              type="text" 
-              value={this.state.firstname} 
-              onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Last Name:
-            <input 
-              name = "lastname"
-              type="text" 
-              value={this.state.lastname}
-              onChange={this.handleChange}/>
-          </label>
-          <br />
-          <label>
-            Email:
-            <input 
-              name = "email"
-              type = "text"
-              value={this.state.email}
-              onChange={this.handleChange}/>
-          </label>
-          <br />
-          <label>
-            Event Password:
-            <input 
-              name = "eventpass"
-              type = "text"
-              value={this.state.eventpass}
-              onChange={this.handleChange}/>
-          </label>
-              <input
-                type="submit"
-                value="Login"
-              />
-            </form>
+            <div className="h-screen">
+              <div className="flex justify-center items-end h-2/5">
+                <img
+                  src={require('assets/images/Blueprints_Logo2.png')} 
+                  alt = "Logo"
+                  className="w-10/12 object-contain"
+                />
+              </div>
+              
+              <div className="flex justify-center items-center py-sm">
+                <h1 className="section_header font-semibold text-dark_blue">Log In</h1>
+              </div>
+
+              <div className="flex justify-center items-start h-2/5">
+                
+                <form 
+                  onSubmit={(event) => this.handleSubmit(event, auth.setLogin)} 
+                  className="w-11/12"
+                >
+                  <div className="flex items-center my-sm whitespace-no-wrap">
+                    <div className="w-1/3">
+                      <label className="block text-right mx-md">
+                        First Name
+                      </label>
+                    </div>
+                    <div className="w-2/3">
+                      <input 
+                        name="firstname"
+                        type="text" 
+                        value={this.state.firstname} 
+                        onChange={this.handleChange} 
+                        placeholder="First"
+                        className="bg-gray-200 border-2 border-gray-200 rounded w-full focus:outline-none focus:bg-white focus:border-light_blue"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center my-sm whitespace-no-wrap">
+                    <div className="w-1/3">
+                      <label className="block text-right mx-md">
+                        Last Name
+                      </label>
+                    </div>
+                    <div className="w-2/3">
+                      <input 
+                        name = "lastname"
+                        type="text" 
+                        value={this.state.lastname}
+                        onChange={this.handleChange}
+                        placeholder="Last"
+                        className="bg-gray-200 border-2 border-gray-200 rounded w-full focus:outline-none focus:bg-white focus:border-light_blue"  
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center my-sm whitespace-no-wrap">
+                    <div className="w-1/3">
+                      <label className="block text-right mx-md">
+                        Email
+                      </label>
+                    </div>
+                    <div className="w-2/3">
+                      <input 
+                        name = "email"
+                        type = "text"
+                        value={this.state.email}
+                        onChange={this.handleChange}
+                        placeholder="email@domain"
+                        className="bg-gray-200 border-2 border-gray-200 rounded w-full focus:outline-none focus:bg-white focus:border-light_blue"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex items-center my-sm whitespace-no-wrap">
+                    <div className="w-1/3">
+                      <label className="block text-right mx-md">
+                        Event Token
+                      </label>
+                    </div>
+                    <div className="w-2/3">
+                      <input 
+                        name = "eventpass"
+                        type = "text"
+                        value={this.state.eventpass}
+                        onChange={this.handleChange}
+                        placeholder="sdkfa2938"
+                        className="bg-gray-200 border-2 border-gray-200 rounded w-full focus:outline-none focus:bg-white focus:border-light_blue"
+                      />
+                    </div>
+                  </div>
+
+                  <div className="flex justify-center pt-md">
+                    <input
+                      type="submit"
+                      value="Login"
+                      className="pill_button w-full"
+                    />
+                  </div>
+                </form>
+              </div>
+            </div>
           )}
         </Authentication.Consumer>
     );
