99461e51fcb3027ee9e2d0a3a17f1dc19d2b5d2a
Unser Hashsham <unserh@umich.edu>
2020-11-02 23:23:59 -0500

    Add instructions to InventoryForm component

--- FrontEnd/src/components/InventoryForm.jsx
+++ FrontEnd/src/components/InventoryForm.jsx
@@ -2,20 +2,50 @@ import React from 'react';
 import FORM_PAGES from 'utils/ScanFormPages';
 
 class InventoryForm extends React.Component {
+  /*
+  Props:
+  - setFormPage() callback function
+  - supplyItem dict containing
+    {
+      id:
+      ref_number:
+      brand:
+      name:
+      category:
+    }
+
+  State: None
+
+  Functions:
+  - back(): sets form page to SupplyList
+  - handleSubmit(): submits form to REST API
+  */
   constructor(props) {
     super(props);
-    this.cancel = this.cancel.bind(this);
+    this.back = this.back.bind(this);
   }
   
-  cancel() {
+  back() {
     this.props.setFormPage(FORM_PAGES.SUPPLY_LIST);
   }
 
+  handleSubmit(formValues) {
+    // TODO: implement this eventually
+  }
+
+  /*
+  TODO: Render a Formik form (see Login.jsx for reference) with the following fields:
+        - Amount
+        - Expiration Day
+        - Expiration Month
+        - Expiration Year
+        Also render a header showing the selected SupplyItem and a back button beneath the form 
+  */
   render() {
     return(
       <div>
         <h1>Inventory Form</h1>
-        <button onClick={this.cancel}>Cancel</button>
+        <button onClick={this.back}>Back</button>
       </div>
     );
   }
