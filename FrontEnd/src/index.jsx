bfa28aca430308948b69de46149a7d89118bc8fb
aashni <aashni@umich.edu>
2020-11-05 02:47:44 -0500

    Supply form roughly completed, still need to test it more thoroughly

--- FrontEnd/src/index.jsx
+++ FrontEnd/src/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import SupplyForm from './components/SupplyForm'
 import './assets/styles/main.css'
 
 ReactDOM.render(
   <App />,
+  // <SupplyForm />,
   document.getElementById('root'),
 );
