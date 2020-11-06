af7f4ecec53614cd1f9514b20c4f1d54e3fb5829
Unser Hashsham <unserh@umich.edu>
2020-11-05 23:21:35 -0500

    Set <App/> to render in index.jsx

--- FrontEnd/src/index.jsx
+++ FrontEnd/src/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import SupplyForm from './components/SupplyForm'
-import InventoryForm from './components/InventoryForm'
 import './assets/styles/main.css'
 
 ReactDOM.render(
-  // <App />,
-  <SupplyForm />,
-  // <InventoryForm />,
+  <App />,
   document.getElementById('root'),
 );
