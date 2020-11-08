0128fab2674c68c9bf23c81860dc95de44b90a33
Unser Hashsham <unserh@umich.edu>
2020-11-08 12:05:46 -0500

    Remove imports to html5 camera

--- FrontEnd/src/components/Camera.jsx
+++ FrontEnd/src/components/Camera.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import Camera from 'react-html5-camera-photo';
 import Webcam from 'react-webcam';
-// import 'react-html5-camera-photo/build/css/index.css';
 
 class CameraFeed extends React.Component {
   /*
