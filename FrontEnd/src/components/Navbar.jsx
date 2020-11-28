a68f6b116ab8a1de6e48af9fe26f07a3d7c43e5e
Unser Hashsham <unserh@umich.edu>
2020-11-28 17:15:47 -0500

    Resize Navbar buttons

--- FrontEnd/src/components/Navbar.jsx
+++ FrontEnd/src/components/Navbar.jsx
@@ -6,9 +6,9 @@ class Navbar extends React.Component {
     return (
       <div className="flex justify-center">
         <ul className="bottom_nav fixed bottom-0"> 
-          <li><NavLink activeClassName="active_link" className="text-light_blue font-bold no-underline p-5 pl-10 pr-10 flex text-3xl" to="/stats"><i className="fas fa-chart-line"></i></NavLink></li>
-          <li><NavLink activeClassName="active_link" className="text-light_blue font-bold no-underline p-5 pl-10 pr-10 flex text-3xl" to="/scan"><i className="fas fa-camera"></i></NavLink></li>
-          <li><NavLink activeClassName="active_link" className="text-light_blue font-bold no-underline p-5 pl-10 pr-10 flex text-3xl" to="/help"><i className="fas fa-question-circle"></i></NavLink></li>
+          <li><NavLink activeClassName="active_link" className="text-light_blue font-bold no-underline py-md px-lg flex text-5xl" to="/stats"><i className="fas fa-chart-line"></i></NavLink></li>
+          <li><NavLink activeClassName="active_link" className="text-light_blue font-bold no-underline py-md px-lg flex text-5xl" to="/scan"><i className="fas fa-camera"></i></NavLink></li>
+          <li><NavLink activeClassName="active_link" className="text-light_blue font-bold no-underline py-md px-lg flex text-5xl" to="/help"><i className="fas fa-question-circle"></i></NavLink></li>
         </ul>
       </div>
     );
